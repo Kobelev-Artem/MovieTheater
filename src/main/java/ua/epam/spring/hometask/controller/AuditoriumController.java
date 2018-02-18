08863b4f951a74734bd89a1e29475eede03dd03b
Artem Kobelev <akobelevbox@gmail.com>
2018-02-19 00:30:00 +0200

    Spring MVC: Adding batch loading using multipart files.

--- src/main/java/ua/epam/spring/hometask/controller/AuditoriumController.java
+++ src/main/java/ua/epam/spring/hometask/controller/AuditoriumController.java
@@ -2,7 +2,7 @@ package ua.epam.spring.hometask.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ua.epam.spring.hometask.domain.Auditorium;
 import ua.epam.spring.hometask.service.AuditoriumService;
@@ -17,7 +17,7 @@ public class AuditoriumController {
     @Resource
     private AuditoriumService auditoriumService;
 
-    @PostMapping
+    @GetMapping
     public String auditoriumPage(Model model){
         Set<Auditorium> auditoriums = auditoriumService.getAll();
         model.addAttribute("auditoriums", auditoriums);
