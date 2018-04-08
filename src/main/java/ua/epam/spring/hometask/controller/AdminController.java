ebd962b69f399178681a158768e8f6877ad1c677
Artem Kobelev <akobelevbox@gmail.com>
2018-04-08 21:23:07 +0300

    Spring MVC 5: add batch loading of events using multipart files.

--- src/main/java/ua/epam/spring/hometask/controller/AdminController.java
+++ src/main/java/ua/epam/spring/hometask/controller/AdminController.java
@@ -1,15 +1,23 @@
 package ua.epam.spring.hometask.controller;
 
+import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ua.epam.spring.hometask.domain.Event;
+import ua.epam.spring.hometask.service.EventService;
+
+import javax.annotation.Resource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @RequestMapping(value = "/admin")
 @Controller
 public class AdminController {
+
+    @Resource
+    private EventService eventService;
 
     @GetMapping
     public String adminPage(){
@@ -17,8 +25,20 @@ public class AdminController {
     }
 
     @PostMapping(value = "/uploadEvent")
-    public String uploadEvent(@RequestParam("file") MultipartFile file){
+    public String uploadEvent(@RequestParam("file") MultipartFile file) throws IOException {
+        Gson gson = new Gson();
+        String eventString = new String(file.getBytes(), StandardCharsets.UTF_8);
+        Event inputEvent = gson.fromJson(eventString, Event.class);
+        eventService.save(inputEvent);
 
         return "admin";
+    }
+
+    public EventService getEventService() {
+        return eventService;
+    }
+
+    public void setEventService(EventService eventService) {
+        this.eventService = eventService;
     }
 }
--- src/main/webapp/WEB-INF/jsp/admin.jsp
+++ src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -5,5 +5,11 @@
 </head>
 <body>
 <h1>Hello, Admin!</h1>
+
+<form method="POST" action="/admin/uploadEvent" enctype="multipart/form-data">
+    <input type="file" name="file" /><br/>
+    <input type="submit" value="Submit" />
+</form>
+
 </body>
 </html>
